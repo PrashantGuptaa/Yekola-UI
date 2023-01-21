79ca97a298d17ad1b93cc6020204a686fdb1c854
PrashantGuptaa <meengineerprashant@gmail.com>
2023-01-22 02:09:05 +0530

    Deleted and formatted files

--- src/containers/RoomList/roomList.js
+++ src/containers/RoomList/roomList.js
@@ -34,9 +34,6 @@ dayjs.extend(customParseFormat);
 
 const RoomList = () => {
   const [showCreateRoomModal, setShowCreateRoomModal] = useState(false);
-  const [roomDetails, setRoomDetails] = useState({});
-  const [errors, setErrors] = useState({});
-  const [showErrors, setShowErrors] = useState({});
   const [isCreatingRoom, setIsCreatingRoom] = useState(false);
   const [roomList, setRoomList] = useState([]);
   const [isroomListLoading, setIsRoomListLoading] = useState(true);
@@ -51,8 +48,6 @@ const RoomList = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const [form] = Form.useForm();
-
   const { product } = routeParams;
 
   const createRoomHeadingAndLabel = `Create new ${product} classroom`;
@@ -65,72 +60,6 @@ const RoomList = () => {
       console.error("Error while verfiying if user can create room", e);
     }
   };
-
-  const handleCreateRoomSubmit = () => {
-    if (checkForErrors()) {
-      return;
-    }
-    createRoom();
-  };
-
-  const checkForErrors = () => {
-    const errorsMessage = {},
-      showErrorMessages = {};
-    const { name, description } = roomDetails;
-    if (!name) {
-      errorsMessage.name = EMPTY_FIELD_ERROR;
-      showErrorMessages.name = true;
-    }
-
-    if (name && name.includes("_")) {
-      errorsMessage.name = RESERVED_CHARACTERS_ERROR;
-      showErrorMessages.name = true;
-    }
-
-    if (!description) {
-      errorsMessage.description = EMPTY_FIELD_ERROR;
-      showErrorMessages.description = true;
-    }
-    setErrors(errorsMessage);
-    setShowErrors(showErrorMessages);
-    if (Object.keys(errorsMessage).length) {
-      return true;
-    }
-    return false;
-  };
-
-  const handleInputChange = (key, value) => {
-    const obj = {
-      ...roomDetails,
-    };
-    obj[key] = value;
-    setRoomDetails(obj);
-  };
-
-  const handleDateAndTimeChange = (key, value, valueStr) => {
-    console.log("F-4", key, value, valueStr);
-    const obj = {
-      ...roomDetails,
-    };
-    obj[key] = value;
-    // obj[`${key}Str`] = JSON.stringify(valueStr);
-
-    setRoomDetails(obj);
-  };
-
-  const handleTimeChange = (key, value) => {
-    console.log("F-4", key, new Date(value));
-    const obj = {
-      ...roomDetails,
-    };
-    obj[key] = value;
-
-    setRoomDetails(obj);
-  };
-
-  console.log("F-4-2", roomDetails);
-
-  // const handleTimeChange
 
   const createRoom = async (roomDetails) => {
     try {
@@ -180,7 +109,7 @@ const RoomList = () => {
           <Spin tip="Loading..." size="large" />
         </div>
       ) : (
-        <div>
+        <>
           <div className="room-list">
             {roomList.map((roomObj) => (
               <RoomBlock
@@ -200,94 +129,15 @@ const RoomList = () => {
               <Title level={3}>No Class Room found</Title>
             </div>
           )}
-        </div>
+        </>
       )}
 
-      <AddRoomModal 
+      <AddRoomModal
         showCreateRoomModal={showCreateRoomModal}
         handleCreateRoomSubmit={createRoom}
         handleCloseCreateRoomModal={() => setShowCreateRoomModal(false)}
         isCreatingRoom={isCreatingRoom}
       />
-
-      {/* <Modal
-        title={createRoomHeadingAndLabel}
-        open={showCreateRoomModal}
-        onOk={handleCreateRoomSubmit}
-        onCancel={() => setShowCreateRoomModal(false)}
-        okButtonProps={{ disabled: isCreatingRoom }}
-        cancelButtonProps={{ disabled: isCreatingRoom }}
-        okText="Submit"
-        confirmLoading={isCreatingRoom}
-        width={400}
-      >
-        <Form
-          onFinish={handleCreateRoomSubmit}
-          //   {...formItemLayout}
-          layout="vertical"
-          form={form}
-        >
-          <InputWithLabel
-            label="Name"
-            value={roomDetails.name}
-            onInputChange={(e) => handleInputChange("name", e.target.value)}
-            placeholder="Enter Room Name"
-            helperText={errors.name}
-            showError={showErrors.name}
-          />
-          <InputWithLabel
-            label="Description"
-            value={roomDetails.description}
-            onInputChange={(e) =>
-              handleInputChange("description", e.target.value)
-            }
-            placeholder="Enter Room Description"
-            helperText={errors.description}
-            showError={showErrors.description}
-          />
-          <div className="date-time">
-            <Space className="vertical">
-              <Text>Start Date</Text>
-              <DatePicker
-                onChange={(dateValue) =>
-                  handleInputChange("startDate", dateValue)
-                }
-                value={roomDetails.date}
-              />
-            </Space>
-            <Space className="vertical">
-              <Text>End Date</Text>
-              <DatePicker
-                onChange={(dateValue) =>
-                  handleInputChange("endDate", dateValue)
-                }
-                value={roomDetails.date}
-              />
-            </Space>
-          </div>
-          <div className="date-time">
-            <Space className="vertical">
-              <Text>Start Time</Text>
-              <TimePicker
-                onChange={(value, str) =>
-                  handleInputChange("startTime", value, str)
-                }
-                value={roomDetails.time}
-                // disabledSeconds={true}
-              />
-            </Space>
-            <Space className="vertical">
-              <Text>End Time</Text>
-              <TimePicker
-                onChange={(value, str) =>
-                  handleInputChange("endTime", value, str)
-                }
-                value={roomDetails.time}
-              />
-            </Space>
-          </div>
-        </Form>
-      </Modal> */}
     </div>
   );
 };
