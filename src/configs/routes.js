db8d824b58556f78fa88ecaf9870a6710362d21b
PrashantGuptaa <meengineerprashant@gmail.com>
2024-01-06 20:45:12 +0530

    deleted unused code

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,7 +1,4 @@
-import Login from "./../containers/Login";
 import SignUser from "./../containers/SignUser";
-import Home from "./../containers/Home";
-import RoomList from "./../containers/RoomList/";
 import InteractiveClass from "../containers/InteractiveClass";
 import Authentication from "../components/Hoc";
 import LandingPage from "../containers/Landing/landing";
@@ -15,20 +12,11 @@ const appRoutes = [
     path: "/sign-user",
     component: SignUser,
   },
-  // {
-  //   path: "/room-list/:product",
-  //   component: Authentication(RoomList),
-  // },
   {
     path: "/class-room/:product/:className/:roomId",
     component: Authentication(InteractiveClass),
   },
-  // {
-  //   path: "/",
-  //   component: Authentication(Home),
-  // },
   {
-    // path: "/le",
     path: "/home/room-list/:product",
     component: Authentication(LandingPage),
   },
